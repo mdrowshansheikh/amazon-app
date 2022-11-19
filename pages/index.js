68b4d236d588f9694b7e130810a417c4748b054d
mdrowshansheikh <mdrowshansheikh008@gmail.com>
2022-11-20 00:01:01 +0600

    06 List Products

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
+import data from '../utils/data';
 
 const Home = () => {
   return (
     <div>
-      <Layout title={'Home Page'}>Home</Layout>
+      <Layout title={'Home Page'}>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => (
+            <ProductItem product={product} key={product.slug}></ProductItem>
+          ))}
+        </div>
+      </Layout>
     </div>
   );
 };
